97160e07c277b2b4971386099b0e6346916e1e35
Ashwin Rohit <ashwinrohit@proton.me>
2023-01-16 14:10:00 -0800

    Complete upto part 1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Content from "./components/Content";
+import Header from "./components/Header";
+import Total from "./components/Total";
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  const parts = [
+    {
+      name: part1,
+      exercises: exercises1,
+    },
+    {
+      name: part2,
+      exercises: exercises2,
+    },
+    {
+      name: part3,
+      exercises: exercises3,
+    },
+  ];
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
